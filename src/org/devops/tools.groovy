c2afca52834c59b5cd4dc817909929fd0380c5c5
zcymantou <2858349172@qq.com>
2024-02-28 15:50:34 +0700

    11111

---
+++ src/org/devops/tools.groovy
@@ -0,0 +1,11 @@
+package org.devops
+
+//格式化输出
+def PrintMes(value,color){
+    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${value} \033[0m"
+            ]
+        ansiColor('xterm') {
+        println(colors[color])
+    }
+}
